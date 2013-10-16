e93f8190ad29268ac9f784c2d7a567097fa01e3a
Takumi Yamamoto <takuyama0901@gmail.com>
2013-10-16 23:48:19 +0900

    Initial Commit

---
+++ PushBingo/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PushBingo
+//
+//  Created by Takumi Yamamoto on 2013/10/16.
+//  Copyright (c) 2013年 Takumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PushBingo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PushBingo
+//
+//  Created by Takumi Yamamoto on 2013/10/16.
+//  Copyright (c) 2013年 Takumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBAppDelegate class]));
+    }
+}
