33cc3002060b0ec547cfc2f71ff64e229c7bae36
mmaruyam <mmaruyam@www1066uj.sakura.ne.jp>
2013-11-03 18:49:13 +0900

    update api files

--- api/conf/query/masterTable/create_table_format.sql
+++ api/conf/query/masterTable/create_table_format.sql
@@ -1,6 +1,7 @@
 CREATE TABLE %s (
   `userid` bigint(20) NOT NULL,
   `status` varchar(16) DEFAULT NULL,
+  `number` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`userid`),
   FOREIGN KEY (`userid`) REFERENCES `user`(`id`)
   ON DELETE CASCADE ON UPDATE CASCADE
