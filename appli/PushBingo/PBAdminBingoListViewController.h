d53790ca23e9f2cd05940319ec99a2f5dabe3b24
mmaruyam <mmaruyam0830@gmail.com>
2013-11-03 17:03:19 +0900

    update admin files

---
+++ appli/PushBingo/PBAdminBingoListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PBAdminBingoListViewController.h
+//  PushBingo
+//
+//  Created by 丸山 三喜也 on 11/3/13.
+//  Copyright (c) 2013 Takumi Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAdminBingoListViewController : UITableViewController
+
+@end
